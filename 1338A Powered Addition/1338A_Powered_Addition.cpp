7e8a5ae990066248348ddf4b44fb1c39b25adeb7
sarojku17 <sarojku.17@gmail.com>
2020-06-21 02:16:13 +0530

    solution to 1338A_Powered_Addition.cpp

--- 1338A Powered Addition/1338A_Powered_Addition.cpp
+++ 1338A Powered Addition/1338A_Powered_Addition.cpp
@@ -14,25 +14,21 @@ int main(int argc, char const *argv[])
         int n;
         cin>>n;
         long long  a[n];
-        long long pre=LLONG_MIN ;
         for(int i=0;i<n;i++)
             cin>>a[i];        
-        long long sol=0;
-        for(int i=0;i<n;i++)
+        long long sol=0,pre=a[0];
+        long long max_diff=0;
+        for(int i=1;i<n;i++)
         {
             if(a[i]<pre){
-                long long diff=pre-a[i];
-                // cout<<"diff "<<diff<<endl;
-                long long cur=ceil(log2(diff+1));
-                // cout<<"cur : "<<i<<" "<<cur<<endl;
-                sol=max(cur,sol);
-                a[i]+=(pow(2,cur)-1);
+                    max_diff=max(max_diff,pre-a[i]);
             }
-            cout<<a[i]<<" ";
-            pre=a[i];
+            pre=max(pre,a[i]);
         }
-        cout<<endl;
-        cout<<sol<<endl;
+        if(max_diff==0)
+            cout<<0<<endl;
+        else
+            cout<<(long)(log2(max_diff))+1<<endl;
 
     }
 
